56bb51b23637432b6b9854d8e2de1f4c578c5c61
Mason McCargish <masonmccargish@gmail.com>
2020-03-05 00:14:18 -0500

    commit all of the current web pages to github

---
+++ client.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php include "sidebar.php" ?>
+
+	<div class="main">
+	  
+	  
+	</div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php include "sidebar.php" ?>
+
+	<div class="main">
+	  <h1>Search</h1>
+
+	  <form>
+		 	<label for="serachbox">Search:</label>
+		 	<input type="text" id="serachbox" name="searchbox">
+		 	<input type="submit" value="Search">
+		</form> 
+	  
+	</div>
+	
+
+</body>
+</html>
